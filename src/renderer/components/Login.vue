6459081372c3afba749b625fa149146ad025da53
Qianzujin <wangjinjian@giti.com>
2020-01-19 16:50:28 +0800

    aa

--- src/renderer/components/Login.vue
+++ src/renderer/components/Login.vue
@@ -42,28 +42,48 @@
       </el-col>
     </el-row>
     <el-row :gutter="0" v-if="mainDisplay">
-      <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
-        <el-tabs :tab-position="tabPosition" style="height: 100%;">
-          <el-tab-pane label="Button 按钮">
-              <ElementButtons></ElementButtons>
-          </el-tab-pane>
-          <el-tab-pane label="Link 文字链接">
-            <ElementTextLinks></ElementTextLinks>
-          </el-tab-pane>
-          <el-tab-pane label="Radio 单选框">
-            <ElementRadios></ElementRadios>
-          </el-tab-pane>
-          <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
-          <el-tab-pane label="用户管理">用户管理</el-tab-pane>
-          <el-tab-pane label="配置管理">配置管理</el-tab-pane>
-          <el-tab-pane label="角色管理">角色管理</el-tab-pane>
-          <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
-          <el-tab-pane label="用户管理">用户管理</el-tab-pane>
-          <el-tab-pane label="配置管理">配置管理</el-tab-pane>
-          <el-tab-pane label="角色管理">角色管理</el-tab-pane>
-          <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
-        </el-tabs>
-      </el-col>
+      <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
+        <el-radio-button :label="false">展开</el-radio-button>
+        <el-radio-button :label="true">收起</el-radio-button>
+      </el-radio-group>
+      <el-menu
+        default-active="1-4-1"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+        :collapse="isCollapse"
+      >
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span slot="title">导航一</span>
+          </template>
+          <el-menu-item-group>
+            <span slot="title">分组一</span>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="1-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="1-4">
+            <span slot="title">选项4</span>
+            <el-menu-item index="1-4-1">选项1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航二</span>
+        </el-menu-item>
+        <el-menu-item index="3" disabled>
+          <i class="el-icon-document"></i>
+          <span slot="title">导航三</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-setting"></i>
+          <span slot="title">导航四</span>
+        </el-menu-item>
+      </el-menu>
     </el-row>
   </div>
 </template>
@@ -72,6 +92,11 @@
 <style scoped>
 @import "./css/main.css";
 @import "./css/font.css";
+
+  .el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
 
 .el-col {
   border-radius: 4px;
@@ -128,7 +153,14 @@ export default {
     },
     handleSelect(key, keyPath) {
       console.log(key, keyPath);
-    }
+    },
+    handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      }, 
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    
   },
   data() {
     return {
@@ -136,7 +168,8 @@ export default {
       mainDisplay: false,
       tabPosition: "left",
       tabPaneDisabled: true,
-      input: ""
+      input: "",
+       isCollapse: true
     };
   }
 };
@@ -188,5 +221,33 @@ export default {
   }
 
 </style>
+
+-->
+
+<!---
+      <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
+        <el-tabs :tab-position="tabPosition" style="height: 100%;">
+          <el-tab-pane  label="测试" disabled >
+           </el-tab-pane>
+          <el-tab-pane label="Button 按钮">
+              <ElementButtons></ElementButtons>
+          </el-tab-pane>
+          <el-tab-pane label="Link 文字链接">
+            <ElementTextLinks></ElementTextLinks>
+          </el-tab-pane>
+          <el-tab-pane label="Radio 单选框">
+            <ElementRadios></ElementRadios>
+          </el-tab-pane>
+          <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
+          <el-tab-pane label="用户管理">用户管理</el-tab-pane>
+          <el-tab-pane label="配置管理">配置管理</el-tab-pane>
+          <el-tab-pane label="角色管理">角色管理</el-tab-pane>
+          <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
+          <el-tab-pane label="用户管理">用户管理</el-tab-pane>
+          <el-tab-pane label="配置管理">配置管理</el-tab-pane>
+          <el-tab-pane label="角色管理">角色管理</el-tab-pane>
+          <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
+        </el-tabs>
+      </el-col>
 
 -->
